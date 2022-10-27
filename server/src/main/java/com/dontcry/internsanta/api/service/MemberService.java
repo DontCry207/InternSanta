5374bcc630a2f20d4ccf556222beb28f592cfc35
양경섭 <yangksks@gmail.com>
2022-10-27 10:47:03 +0900

    Revert "Merge branch 'server' into 'master'"
    This reverts merge request !1

--- server/src/main/java/com/dontcry/internsanta/api/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.dontcry.internsanta.api.service;
-
-import com.dontcry.internsanta.db.entity.Member;
-
-public interface MemberService {
-    public Member getMemberByMemberEmail(String memberEmail);
-}
