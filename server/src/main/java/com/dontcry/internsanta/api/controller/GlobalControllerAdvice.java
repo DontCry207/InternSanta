2754a9f22e053968dad84326763628cdf89da7c1
김혜성 <syong0716@naver.com>
2022-10-31 11:10:30 +0900

    Feat: 운세 보기 api 구현

---
+++ server/src/main/java/com/dontcry/internsanta/api/controller/GlobalControllerAdvice.java
@@ -0,0 +1,48 @@
+package com.dontcry.internsanta.api.controller;
+
+import com.dontcry.internsanta.common.exception.code.ErrorCode;
+import com.dontcry.internsanta.common.exception.member.MemberNicknameValidateException;
+import com.dontcry.internsanta.common.exception.member.MemberNotFoundException;
+import com.dontcry.internsanta.common.exception.member.MemberUnauthorizedException;
+import com.dontcry.internsanta.common.exception.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("handleException", e);
+        ErrorResponse response = new ErrorResponse(ErrorCode.INTER_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // 멤버
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleMemberNotFoundException(MemberNotFoundException e) {
+        log.error("handleMemberNotFoundException", e);
+        ErrorResponse response = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    // 토큰 없는 경우 (401 Unauthorized)
+    @ExceptionHandler(MemberUnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleMemberUnauthorizedException(MemberUnauthorizedException e) {
+        log.error("handleMemberUnauthorizedException", e);
+        ErrorResponse response = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(MemberNicknameValidateException.class)
+    public ResponseEntity<ErrorResponse> handleMemberNicknameValidateException(MemberNicknameValidateException e) {
+        log.error("handleMemberNicknameValidateException", e);
+        ErrorResponse response = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
+
+}
