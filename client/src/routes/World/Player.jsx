328add522cdf36f4c504d13961933346b0576cd1
원재호 <wonjh07@naver.com>
2022-11-04 13:48:50 +0900

    Feat: 3인칭 캐릭터 방향전환
    Former-commit-id: cddb195e448ec9b9bdf6d6d14f9d006e1403a5e4

--- client/src/routes/World/Player.jsx
+++ client/src/routes/World/Player.jsx
@@ -1,40 +1,71 @@
-import { useRef, useState } from 'react';
-import { useLoader, useThree, useFrame, extend } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useEffect, useRef, useState } from 'react';
+import { useThree, useFrame, extend } from '@react-three/fiber';
 import ilbuni from '../../assets/ilbuni.glb';
-import { RigidBody, BallCollider } from '@react-three/rapier';
+import MainCharacter from '../../assets/MainCharacter.glb';
+import { RigidBody, CuboidCollider } from '@react-three/rapier';
 import {
   OrbitControls,
   MapControls,
 } from 'three/examples/jsm/controls/OrbitControls';
 import * as THREE from 'three';
-import { useKeyboardControls } from '@react-three/drei';
+import { useKeyboardControls, useGLTF, useAnimations } from '@react-three/drei';
 extend({ OrbitControls, MapControls });
 
 const Player = () => {
-  const gltf = useLoader(GLTFLoader, ilbuni);
-  gltf.scene.scale.set(0.5, 0.5, 0.5);
-  const SPEED = 5;
+  const SPEED = 4;
   const direction = new THREE.Vector3();
   const frontVector = new THREE.Vector3();
   const sideVector = new THREE.Vector3();
   const ref = useRef();
+
   const {
     camera,
     gl: { domElement },
+    scene,
   } = useThree();
   const [, get] = useKeyboardControls();
-  const [location, setLocation] = useState([0, 2, 2]);
+  const [location, setLocation] = useState([10, 10, -2]);
+  const [location2, setLocation2] = useState([10, 10, -2]);
   const controls = useRef();
+
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF(ilbuni);
+  const { actions } = useAnimations(animations, group);
+  nodes.Armature.rotation.copy(camera.rotation);
 
   useFrame((state, delta) => {
     const { forward, backward, left, right } = get();
     const velocity = ref.current.linvel();
     // update camera
     const [x, y, z] = [...ref.current.translation()];
-    setLocation([x, y + 0.8, z]);
-    controls.current.update();
+    setLocation([x, y + 0.6, z]);
+    setLocation2([x, y - 0.1, z]);
 
+    if (forward || backward || left || right) {
+      actions.default.stop();
+      actions.walk.play().setEffectiveTimeScale(1.3);
+    } else {
+      actions.walk.stop();
+      actions.default.play();
+    }
+
+    nodes.Armature.rotateY(Math.PI);
+    if (forward && left) {
+      nodes.Armature.rotateY(Math.PI / 4);
+    } else if (backward && left) {
+      nodes.Armature.rotateY((3 * Math.PI) / 4);
+    } else if (left) {
+      nodes.Armature.rotateY(Math.PI / 2);
+    } else if (forward && right) {
+      nodes.Armature.rotateY(-Math.PI / 4);
+    } else if (backward && right) {
+      nodes.Armature.rotateY((-3 * Math.PI) / 4);
+    } else if (right) {
+      nodes.Armature.rotateY(-Math.PI / 2);
+    } else if (backward) {
+      nodes.Armature.rotateY(Math.PI);
+    }
+    controls.current.update();
     // movement
     frontVector.set(0, 0, backward - forward);
     sideVector.set(left - right, 0, 0);
@@ -48,25 +79,36 @@ const Player = () => {
 
   return (
     <>
-      <mapControls
+      <orbitControls
         ref={controls}
+        makeDefaults
         args={[camera, domElement]}
         target={location}
-        minDistance={2}
-        maxDistance={2}
-        maxPolarAngle={Math.PI / 2.7}
-        minPolarAngle={Math.PI / 2.7}
+        minDistance={1.6}
+        maxDistance={1.6}
+        maxPolarAngle={Math.PI / 2.6}
+        minPolarAngle={Math.PI / 2.6}
+        enableRotate={false}
+        enablePan={false}
+      />
+      <primitive
+        ref={group}
+        object={nodes.Armature}
+        position={location2}
+        scale={(0.2, 0.2, 0.2)}
       />
       <RigidBody
         ref={ref}
+        mass={1}
         type="dynamic"
         colliders={false}
-        position={[0, 3, 0]}>
-        <primitive object={gltf.scene} />
-        <BallCollider args={[0.3, 0.3]} />
+        position={[0, 2, 0]}>
+        <CuboidCollider args={[0.3, 0.3, 0.3]} />
       </RigidBody>
     </>
   );
 };
+
+useGLTF.preload(ilbuni);
 
 export default Player;
