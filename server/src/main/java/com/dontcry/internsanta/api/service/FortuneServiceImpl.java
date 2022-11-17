57e07d328f60af2863b583273af19ebc97bd7d10
김혜성 <syong0716@naver.com>
2022-11-18 00:30:36 +0900

    Fix: 운세 버그 수정
    Former-commit-id: 7b56af8b91c2dcf6554f11a4f132db9955aac72f

--- server/src/main/java/com/dontcry/internsanta/api/service/FortuneServiceImpl.java
+++ server/src/main/java/com/dontcry/internsanta/api/service/FortuneServiceImpl.java
@@ -20,9 +20,9 @@ public class FortuneServiceImpl implements FortuneService{
     public Fortune getRandomFortune(Long memberId) {
         int fortuneCnt = fortuneRepository.countFortunes();
         Long seed = (LocalDateTime.now().getMonthValue() + LocalDateTime.now().getDayOfMonth() + memberId) % fortuneCnt;
-        Random random = new Random(seed.intValue());
-        int fortuneId = random.nextInt(seed.intValue());
-        System.out.println(fortuneId);
+        Random random = new Random(Math.abs(seed.intValue()));
+        int fortuneId = random.nextInt(Math.abs(seed.intValue()));
+        if(fortuneId == 0) fortuneId = 1;
         return fortuneRepository.findByFortuneId(new Long(fortuneId)).orElseThrow(() -> new FortuneNotFoundException("fortune not found", ErrorCode.FORTUNE_NOT_FOUND));
     }
 }
